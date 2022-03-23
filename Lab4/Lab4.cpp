ad6778f3e0b7fa54f93b532015d8383e455641e5
sdanilov <sddanilov_1@edu.hse.ru>
2022-03-23 13:42:20 +0300

    Initial structure

--- Lab4/Lab4.cpp
+++ Lab4/Lab4.cpp
@@ -1,152 +1,97 @@
-﻿#include <mpi.h>
-
-
-
-int odd_even_sort(double** tab_ptr, int p, int P, int N)
-{
-    double* x = *tab_ptr;
-    int n = 1000;
-    //Start by sorting the data locally
-
-    qsort((void*)x, n, sizeof(double), compareDouble);
-
-
-
-    //Number of data on processes with which we communicate
-
-    int right_n = 0;
-
-    int left_n = 0;
-
-
-
-    if (p + 1 < P)
-
-        right_n = data_size(p + 1, P, N);
-
-    if (p - 1 > -1)
-
-        left_n = data_size(p - 1, P, N);
-
-
-
-    int max_size = (right_n > left_n) ? right_n : left_n;
-
-
-
-    double* a = (double*)malloc(max_size * sizeof(double));
-    double* b = (double*)malloc(n * sizeof(double));
-
-    /////////////////////////
-
-    // ODD-EVEN ITERATIONS
-
-    /////////////////////////
-
-    int evenPhase = true;
-
-    int evenNode = p % 2;
-
-
-
-    for (int step = 0; step < P; step++)
-
-    {
-        MPI_Status status;
-
-        //Even nodes communicate with the process on their right in even phases
-
-        //Odd nodes communicate with the process on their right in odd phases
-
-        if (evenPhase && evenNode || !evenPhase && !evenNode)
-
-        {
-
-            //if right process exists
-
-            if (p + 1 < P)
-
-            {
-
-                //Exchange data
-
-                MPI_Send(x, n, MPI_DOUBLE, p + 1, 42, MPI_COMM_WORLD);
-
-                MPI_Recv(a, right_n, MPI_DOUBLE, p + 1, 43, MPI_COMM_WORLD,
-
-                    &status);
-
-
-
-                //Merge the sorted table, keep the first n data and
-
-                //put it in b.
-
-                merge_sorted_table(x, n,
-
-                    a, right_n,
-
-                    b, n, true);
-
-                double* temp = x;
-
-                x = b;
-
-                b = temp;
-
-            }
-
-        }
-
-        else //communicate with process on the left
-
-        {
-
-            //if left process exists
-
-            if (p - 1 > -1)
-
-            {
-
-                //Exchange data
-
-                MPI_Recv(a, left_n, MPI_DOUBLE, p - 1, 42, MPI_COMM_WORLD,
-
-                    &status);
-
-                MPI_Send(x, n, MPI_DOUBLE, p - 1, 43, MPI_COMM_WORLD);
-
-
-
-                //Merge the sorted tables, keep the last n data and
-
-                //put it in b.
-
-                merge_sorted_table(x, n, a, left_n, b, n, false);
-
-                double* temp = x;
-
-                x = b;
-
-                b = temp;
-
-            }
-
-        }
-
-        //Next phase
-
-        evenPhase = !evenPhase;
-
-    }
-
-
-
-    *tab_ptr = x;
-
-    free(a); free(b);
-
-    return n;
-
+﻿#define N 16
+
+#include <stdlib.h>
+#include <iostream>
+#include "mpi.h"
+#include "math.h"
+
+int size, rank, ndim, subvector_size;
+int ndims[2] = { 0,0 };
+int period[2] = { 0, 0 };
+int coords[2];
+MPI_Comm com, row_comm, col_comm;
+
+double* vectorTemp;
+double* vectorOriginal;
+
+void print_vector(int s, double* vectort) {
+	for (int i = 0; i < s; i++) {
+		std::cout << vectort[i] << " ";
+	}
+	std::cout << std::endl;
+}
+
+void prepare_block(int a, double* tVector, double* tVectorOrig) {
+	for (int i = a*subvector_size; i < a * subvector_size + subvector_size; i++) {
+		tVector[i-a * subvector_size] = tVectorOrig[i];
+	}
+}
+
+void populate_block(int a, double* tVector, double* tVectorOrig) {
+	for (int i = a * subvector_size; i < a * subvector_size + subvector_size; i++) {
+		tVectorOrig[i] = tVector[i - a * subvector_size];
+	}
+}
+
+int main(int argc, char** argv) {
+	MPI_Init(&argc, &argv);
+	MPI_Comm_size(MPI_COMM_WORLD, &size);
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	double start = MPI_Wtime();
+	subvector_size = N / size;
+
+
+	//prepare data
+	if (rank == 0) {
+		vectorOriginal = new double[N * N];
+		for (int i = 0; i < N; i++) {
+			vectorOriginal[i] = 5;
+		}
+	}
+
+	vectorTemp = new double[subvector_size * subvector_size];
+	for (int i = 0; i < subvector_size; i++) {
+		vectorTemp[i] = 0;
+	}
+	MPI_Status mStatus;
+
+	// v distribution for all processes
+	if (rank == 0) {
+		for (int i = 0; i < size; i++) {
+			if (i == 0) continue;
+			prepare_block(i,  vectorTemp, vectorOriginal);
+			MPI_Send(vectorTemp, subvector_size, MPI_DOUBLE, i, 1, MPI_COMM_WORLD);
+		}
+		prepare_block(0, vectorTemp, vectorOriginal);
+	}
+
+	// receive v
+	if (rank != 0) {
+		MPI_Recv(vectorTemp, subvector_size, MPI_DOUBLE, 0, 1, MPI_COMM_WORLD, &mStatus);
+	}
+
+	// Do some shit with vector
+
+	// send v'
+	if (rank != 0) {
+		print_vector(subvector_size, vectorTemp);
+		MPI_Send(vectorTemp, subvector_size, MPI_DOUBLE, 0, 1, MPI_COMM_WORLD);
+	}
+
+	// gather and construct C matrix
+	if (rank == 0) {
+		populate_block(0, vectorTemp, vectorOriginal);
+		for (int i = 0; i < size; i++) {
+			if (i == 0) continue;
+			MPI_Recv(vectorTemp, subvector_size, MPI_DOUBLE, i, 1, MPI_COMM_WORLD, &mStatus);
+			populate_block(i, vectorTemp, vectorOriginal);
+		}
+		delete[] vectorOriginal;
+		double end = MPI_Wtime();
+		std::cout << end - start;
+	}
+
+	MPI_Finalize();
+	return 0;
 }
 
